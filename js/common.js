aec5d117dd3bc2a77aa30b6b2eaef5c91a43c98b
sunbey <544712159@qq.com>
2016-11-14 16:58:18 +0800

    Complete Index Page
    Complete Index Page for this platform

--- js/common.js
+++ js/common.js
@@ -59,4 +59,70 @@ $(function(){
 		})
 	})
 
-})
+});
+
+//News scroll
+(function($){
+	$.fn.extend({
+	    Scroll:function(opt,callback){
+	        if(!opt) var opt={};
+	        var _btnUp = $("#"+ opt.up);
+	        var _btnDown = $("#"+ opt.down);
+	        var direct = opt.direct || "height"; //height width
+	        if(direct !== "height" && direct !== "width") direct = "height";
+	        var marginObj,marginInitObj;
+	        var timerID;
+	        var _this=this.eq(0).find("ul:first");
+	        var     lineH=parseInt(_this.find("li:first").css(direct),10),  //width
+	                line=opt.line?parseInt(opt.line,10):parseInt(this.height()/lineH,10), 
+	                speed=opt.speed?parseInt(opt.speed,10):500; 
+	                timer=opt.timer 
+
+	        if(line==0) line=1;
+	        var upHeight=0-line*lineH;
+
+	        if(direct === "height"){
+	        	marginObj = {marginTop:upHeight}
+	        	marginInitObj = {marginTop:0}
+	        }  
+	        if(direct === "width"){
+	        	marginObj = {marginLeft:upHeight} 
+	        	marginInitObj = {marginLeft:0}
+	        } 
+
+	        var scrollUp=function(){
+	                _btnUp.unbind("click",scrollUp); 
+	                _this.animate(marginObj,speed,function(){
+	                        for(i=1;i<=line;i++){
+	                                _this.find("li:first").appendTo(_this);
+	                        }
+	                        _this.css(marginInitObj); //marginLeft
+	                        _btnUp.bind("click",scrollUp); 
+	                });
+	        }
+
+	        var scrollDown=function(){
+	                _btnDown.unbind("click",scrollDown);
+	                for(i=1;i<=line;i++){
+	                        _this.find("li:last").show().prependTo(_this);
+	                }
+	                _this.css(marginObj); //marginLeft
+	                _this.animate(marginInitObj,speed,function(){
+	                        _btnDown.bind("click",scrollDown);
+	                });
+	        }
+
+	        var autoPlay = function(){
+	                if(timer)timerID = window.setInterval(scrollUp,timer);
+	        };
+	        var autoStop = function(){
+	                if(timer)window.clearInterval(timerID);
+	        };
+
+	        _this.hover(autoStop,autoPlay).mouseout();
+	        _btnUp.click( scrollUp ).hover(autoStop,autoPlay);
+	        _btnDown.click( scrollDown ).hover(autoStop,autoPlay);
+
+	    }       
+	})
+})(jQuery);
